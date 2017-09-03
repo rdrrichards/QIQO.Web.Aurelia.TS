b60b39a27f039531fca588c09a2d86620fd83166
rdrrichards@hotmail.com <rdrrichards@hotmail.com>
2017-09-03 11:10:58 -0500

    finder clean up

--- src/products/find-product.ts
+++ src/products/find-product.ts
@@ -2,21 +2,16 @@ import { Router } from 'aurelia-router';
 import { IProductPage } from './../models/product-page';
 import { IProduct } from './../models/product';
 import { ProductService } from './../services/product.service';
-import { IOrder } from './../models/order';
 import { autoinject } from 'aurelia-dependency-injection';
 
 @autoinject
 export class FindProduct {
   message = 'Find Product';
-  orders: IOrder[];
   pattern = '';
   page: IProductPage;
 
   constructor(private productService: ProductService,
     private router: Router) { }
-
-  // activate() {
-  // }
 
   find() {
     console.log('About to run the find product function with value: ' + this.pattern);
@@ -26,5 +21,9 @@ export class FindProduct {
 
   onSelectProduct(event: UIEvent, product: IProduct) {
     this.router.navigateToRoute('product-detail', { id: product.productKey })
+  }
+
+  next(page: number, pageSize: number, orderBy: string, category: string) {
+
   }
 }
